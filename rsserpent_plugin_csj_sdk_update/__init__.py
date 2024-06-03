e285e7231718e1ef7345a3d8b4a32a10c6faa0d9
EkkoG <beijiu572@gmail.com>
2024-06-03 11:59:27 +0800

    fix: pub_date type error

--- rsserpent_plugin_csj_sdk_update/__init__.py
+++ rsserpent_plugin_csj_sdk_update/__init__.py
@@ -2,7 +2,6 @@ from rsserpent_rev.models import Persona, Plugin
 
 from . import route
 
-
 plugin = Plugin(
     name="rsserpent-plugin-csj-sdk-update",
     author=Persona(
--- rsserpent_plugin_csj_sdk_update/route.py
+++ rsserpent_plugin_csj_sdk_update/route.py
@@ -1,15 +1,17 @@
 import json
 import os
-from typing import Any, Dict, cast
+from typing import Any, cast
 
+import arrow
 from rsserpent_rev.utils import HTTPClient, cached
-
 
 path = "/csj-sdk-update/{platform:int}"
 
+source = "https://www.csjplatform.com/union/media/union/download/log?id=143&cate=1&backPath=/union/media/union/download/groMore"  # noqa: E501
+
 
 @cached
-async def provider(platform: int) -> Dict[str, Any]:
+async def provider(platform: int) -> dict[str, Any]:
     """Return the update logs of CSJ SDK."""
     async with HTTPClient() as client:
         cookies = {
@@ -20,6 +22,7 @@ async def provider(platform: int) -> Dict[str, Any]:
             "https://www.csjplatform.com/union_pangle/api/access_document/list?",
             cookies=cookies,
         )
+
         logs = list(
             filter(lambda x: x["Id"] == platform, response.json()["data"]["list"])
         )[0]
@@ -34,7 +37,7 @@ async def provider(platform: int) -> Dict[str, Any]:
                     "title": f"{logs['Name']} {log['UpdateVersion']} 更新",
                     "description": log["UpdateInfo"],
                     "link": f"https://www.csjplatform.com/union/media/union/download/log?id={platform}",
-                    "pub_date": log["UpdateDate"],
+                    "pub_date": arrow.get(log["UpdateDate"]),
                 }
                 for log in json.loads(logs["Log"])
             ],
